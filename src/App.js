bdaea70d51cdb5416bd44f9d804d8c9e5f9554c7
shazwendy <akothsharon19@gmail.com>
2019-10-03 19:37:16 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Home from './components/Home'
+import Blog from './components/Blog'
+import Gallery from './components/Gallery'
+import Contact from './components/Contact'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    render () {
+        return (
+          <div className = "is-preload">
+            <div id = "wrapper">
+          <Home/>
+          <Blog/>
+          <Gallery/>
+          <Contact/>
+          <div class="copyright">&copy; Shaz cafe. All rights reserved.<a href="https://html5up.net"></a>.</div>
+          </div>
+        </div>
+        )
+    }
 }
 
-export default App;
+
+
+export default App
